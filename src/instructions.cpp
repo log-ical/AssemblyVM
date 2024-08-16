a751339147f4e87b8922b4e6161686f5bca52870
Ryan W <42725021+IsThatLogic@users.noreply.github.com>
2024-08-16 17:03:50 -0400

    instructions

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -1,2 +1,68 @@
 #include "instructions.h"
+#include <stdexcept>
+#include <cstdint>
+#include <sstream>
 
+void Instructions::loadInstructions(const std::vector<std::string>& program){
+    this->program = program;
+}
+void Instructions::execute(){
+    while(clock<program.size()){
+        exeInstruction(program[clock]);
+        ++clock;
+    }
+}
+void Instructions::exeInstruction(const std::string& instruction){
+    std::istringstream iss(instruction);
+    std::string op;
+    iss >> op;
+    if(op == "MOV"){
+        std::string reg;
+        uint8_t value;
+        iss >> reg >> value;
+        uint16_t address = regAddress(reg);
+        mov(address, value);
+    }
+    else if(op == "PUSH"){
+
+    }
+    else if(op == "POP"){
+
+    }
+    else if(op == "ADD"){
+        
+    }
+    else if(op == "SUB"){
+        
+    }
+    else if(op == "PRINT"){
+        
+    }
+}
+
+void Instructions::print(const uint16_t reg){
+
+}
+void Instructions::mov(uint16_t& reg, int value){
+    memory.store(reg, value);
+}
+void Instructions::push(uint16_t& reg){
+
+}
+void Instructions::pop(uint16_t& reg){
+
+}
+
+void Instructions::add(uint16_t& reg1, uint16_t& reg2){
+
+}
+void Instructions::sub(uint16_t& reg1, uint16_t& reg2){
+
+}
+uint16_t Instructions::regAddress(std::string& reg){
+    if(reg == "EAX") return 0x0000;
+    if(reg == "EBX") return 0x0004;
+    if(reg == "ECX") return 0x0008;
+    if(reg == "EDX") return 0x000C;
+    throw std::invalid_argument("Register not valid");
+}
--- src/instructions.h
+++ src/instructions.h
@@ -1,44 +1,52 @@
 #include <vector>
 #include <cstdint>
+#include <string>
+#include "memory.cpp"
 
 class Instructions {
 public:
-
-
+    Instructions():clock(0){}
+    void loadInstructions(const std::vector<std::string>& program);
+    void execute();
 private:
+    Memory memory;
+    std::vector<std::string> program;
+    uint32_t clock;
+    uint16_t regAddress(std::string& reg);
+    void exeInstruction(const std::string& instruction);
     void print(const uint16_t reg);
     void mov(uint16_t& reg, int value);
     void push(uint16_t& reg);
-    void pop();
+    void pop(uint16_t& reg);
     // void lea();
     //arithm
-    void add();
-    void sub();
-    void inc();
-    void dec();
-    void imul();
-    void idiv();
-    //bitwise
-    void _and();
-    void _or();
-    void _xor();
-    void _not();
-    void neg();
-    void shl();
-    void shr();
-    //flow instruction
-    void jmp();
-    //condtional jumps
-    void je();
-    void jne();
-    void jz();
-    void jg();
-    void jge();
-    void jl();
-    void jle();
-
-    void cmp();
-    void call();
-    void ret();
+    void add(uint16_t& reg1, uint16_t& reg2);
+    void sub(uint16_t& reg1, uint16_t& reg2);
+    //void inc();
+    //void dec();
+    //void imul();
+    //void idiv();
+    ////bitwise
+    //void _and();
+    //void _or();
+    //void _xor();
+    //void _not();
+    //void neg();
+    //void shl();
+    //void shr();
+    ////flow instruction
+    //void jmp();
+    ////condtional jumps
+    //void je();
+    //void jne();
+    //void jz();
+    //void jg();
+    //void jge();
+    //void jl();
+    //void jle();
+//
+    //void cmp();
+    //void call();
+    //void ret();
 
 };
